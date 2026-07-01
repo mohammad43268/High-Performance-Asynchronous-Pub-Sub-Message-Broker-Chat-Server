5dc4e96d273cb9c015d714ed5b300f47bbdc11b3
mohammad43268 <huzaifa2086@gmail.com>
2026-07-02 03:23:42 +0530

    Test: Initial C++ environment sanity check

---
+++ main.cpp
@@ -0,0 +1,11 @@
+echo '#include <iostream>
+
+    int
+    main()
+{
+  std::cout << "=========================================" << std::endl;
+  std::cout << "  C++ Environment is working perfectly!  " << std::endl;
+  std::cout << "=========================================" << std::endl;
+  return 0;
+}
+' > main.cpp
